a23fca2ac5c3fe052cda918149925d621da259a9
Coltin Lappin-Lux <coltin.lappin-lux@silverlinecrm.com>
2019-09-19 11:30:14 -0500

    Print Data to Summary Sheet

--- excel_data_sum.py
+++ excel_data_sum.py
@@ -17,7 +17,7 @@ class ReportHeader:
         self.companyTitle = ''
         self.reportType = ''
         self.reportPeriod = ''
-        self.preparationDate = "Report Preparation Date: " + date.today().strftime("%m/%d/%Y")
+        self.preparationDate = date.today().strftime("%m/%d/%Y")
     
     def setCompanyTitle(self, companyTitle):
         self.companyTitle = companyTitle
@@ -25,8 +25,19 @@ class ReportHeader:
     def setReportType(self, reportType):
         self.reportType = reportType
     
-    def setReportPeriod(self, reportPeriod):
-        self.reportPeriod = reportPeriod
+    def setReportPeriod(self, reportPeriodStart, reportPeriodEnd):
+        self.reportPeriod = reportPeriodStart + ' - ' + reportPeriodEnd
+    
+    def getHeader(self):
+        result = []
+        if self.companyTitle != '':
+            result.append(self.companyTitle)
+        if self.reportType != '':
+            result.append(self.reportType)
+        if self.reportPeriod != ' - ':
+            result.append(self.reportPeriod)
+        result.append(self.preparationDate)
+        return result
 
 #==================================================================
 # Metadata
@@ -57,25 +68,79 @@ class ReportContent:
     
     def generateResults(self):
         if(self.primaryColumn == self.summaryColumn):
-            return 'Same'
+            self.primaryColumnTitle = self.dataSheet[self.primaryColumn + '1'].value
+            values = []
+            unduplicateValues = []
+            index = 2
+            primaryIndex = self.primaryColumn + str(index)
+            while self.dataSheet[primaryIndex].value != None:
+                value = self.dataSheet[primaryIndex].value
+                values.append(value)
+                if unduplicateValues.count(value) == 0:
+                    unduplicateValues.append(value)
+                index += 1
+                primaryIndex = self.primaryColumn + str(index)
+            self.result = [[self.primaryColumnTitle]]
+            totalCount = len(values)
+            totalPercentage = 0.00
+            for value in unduplicateValues:
+                count = values.count(value)
+                percentage = round((count/totalCount),2)
+                totalPercentage += percentage
+                self.result.append([[value,'Count'],count])
+                self.result.append([[value,'Percentage'],percentage])
+            self.result.append([['Total','Count'],totalCount])
+            self.result.append([['Total','Percentage'],totalPercentage])
+            return self.result
         else:
             self.primaryColumnTitle = self.dataSheet[self.primaryColumn + '1'].value
             self.summaryColumnTitle = self.dataSheet[self.summaryColumn + '1'].value
-            return 'Not Same'
+            valuePairs = []
+            unduplicateValuePairs = []
+            summaryValues = []
+            unduplicateSummaryValues = []
+            index = 2
+            primaryIndex = self.primaryColumn + str(index)
+            summaryIndex = self.summaryColumn + str(index)
+            while self.dataSheet[primaryIndex].value != None:
+                key = self.dataSheet[primaryIndex].value
+                value =  self.dataSheet[summaryIndex].value
+                valuePairs.append([key,value])
+                summaryValues.append(value)
+                if unduplicateValuePairs.count([key,value]) == 0:
+                    unduplicateValuePairs.append([key,value])
+                if unduplicateSummaryValues.count(value) == 0:
+                    unduplicateSummaryValues.append(value)
+                index += 1
+                primaryIndex = self.primaryColumn + str(index)
+                summaryIndex = self.summaryColumn + str(index)
+            self.result = [[self.summaryColumnTitle, self.primaryColumnTitle]]
+            for pair in unduplicateValuePairs:
+                count = valuePairs.count(pair)
+                self.result.append([pair,count])
+            totalCount = len(valuePairs)
+            for value in unduplicateSummaryValues:
+                count = summaryValues.count(value)
+                percentage = round((count/totalCount),2)
+                self.result.append([['Total (#)',value],count])
+                self.result.append([['Total (%)',value],percentage])
+            return self.result
 
 #==================================================================
 # Report
 #==================================================================
 class Report:
     def __init__(self):
-        self.header = ReportHeader()
+        self.header = []
         self.metadata = Metadata()
         self.content = []
     
     def setHeader(self):
-        self.header.setCompanyTitle(raw_input("Company: "))
-        self.header.setReportType("Report Type: " + raw_input("Report Type: "))
-        self.header.setReportPeriod("Report Period: " + raw_input("Report Start Date: ") + " - " + raw_input("Report End Date: "))
+        header = ReportHeader()
+        header.setCompanyTitle(raw_input("Company: "))
+        header.setReportType(raw_input("Report Type: "))
+        header.setReportPeriod(raw_input("Report Start Date: "), raw_input("Report End Date: "))
+        self.header = header.getHeader()
     
     def setMetadata(self):
         self.metadata.setPrimaryAttribute(raw_input("Primary Attribute Column: "))
@@ -92,7 +157,73 @@ class Report:
             columnContent = ReportContent(self.metadata.primaryAttribute, col, self.dataSheet)
             results = columnContent.generateResults()
             self.content.append(results)
-        print self.content
+    
+    def printReportToConsole(self, withHeader, withContent):
+        if withHeader:
+            print self.header
+        if withContent:
+            for content in self.content:
+                print ' '
+                for data in content:
+                    print data
+    
+    def printReport(self, withHeader, withContent):
+        index = 1
+        loc = 'A' + str(index)
+        if withHeader:
+            for item in self.header:
+                self.summarySheet[loc] = item
+                index += 1
+                loc = 'A' + str(index)
+            index += 1
+            loc = 'A' + str(index)
+        number = index
+        loc = 'A' + str(number)
+        if withContent:
+            for contentItem in self.content:
+                contentTitle = contentItem.pop(0)
+                loc = 'A' + str(number)
+                for item in contentTitle:
+                    self.summarySheet[loc] = item
+                    number += 1
+                    loc = 'A' + str(number)
+                
+                #START
+                letter = 'A'
+                number = number - 1
+                startLoc = letter + str(number)
+
+                #ROW
+                rowNumber = number + 1
+                rowLoc = letter + str(rowNumber)
+
+                #COLUMN
+                colLetter = 'B'
+                colLoc = colLetter + str(number)
+
+                columns = []
+                columnIndexList = []
+                rows = []
+                rowIndexList = []
+                
+                for data in contentItem:
+                    if data[0][0] not in rows:
+                        rows.append(data[0][0])
+                        rowIndexList.append(rowNumber)
+                        self.summarySheet[rowLoc] = data[0][0]
+                        rowNumber += 1
+                        rowLoc = letter + str(rowNumber)
+                    if data[0][1] not in columns:
+                        columns.append(data[0][1])
+                        columnIndexList.append(colLetter)
+                        self.summarySheet[colLoc] = data[0][1]
+                        colLetter = chr(ord(colLetter) + 1)
+                        colLoc = colLetter + str(number)
+                    printNum = rowIndexList[rows.index(data[0][0])]
+                    printCol = columnIndexList[columns.index(data[0][1])]
+                    printLoc = printCol + str(printNum)
+                    self.summarySheet[printLoc] = data[1]
+                number = rowNumber + 1
 
 
 
@@ -131,7 +262,7 @@ def main():
     # Create Report Object & Load Report Header
     #==================================================================
     print '\nLoading Report Header ...'
-    #report.setHeader()
+    report.setHeader()
 
     #==================================================================
     # Load Report Metadata
@@ -144,6 +275,7 @@ def main():
     #==================================================================
     print '\nGenerating Report ...'
     report.generateContent()
+    report.printReport(True,True)
 
     #==================================================================
     # Save Workbook
